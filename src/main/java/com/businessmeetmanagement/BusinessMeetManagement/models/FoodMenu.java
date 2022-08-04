a82c70f4d655ea14d065e29154cef762d856ad54
Sattish <sattishyadav03@gmail.com>
2022-08-04 10:44:13 +0530

    added FoodMenu

---
+++ src/main/java/com/businessmeetmanagement/BusinessMeetManagement/models/FoodMenu.java
@@ -0,0 +1,25 @@
+package com.businessmeetmanagement.BusinessMeetManagement.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodMenu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int foodMenuId;
+    private String foodMenuName;
+    private String foodMenuImageUrl;
+    private long FoodMenuPrice;
+    private String itemCategory;
+}
+
