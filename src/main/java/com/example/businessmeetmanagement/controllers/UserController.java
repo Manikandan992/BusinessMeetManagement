90a9eb4eaac75c2952d1fc6eba9cbe386f47875e
Manikandan992 <manikandan.ad.99@gmail.com>
2022-08-10 09:55:02 +0530

    added cross origin

--- src/main/java/com/example/businessmeetmanagement/controllers/UserController.java
+++ src/main/java/com/example/businessmeetmanagement/controllers/UserController.java
@@ -16,10 +16,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,6 +25,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/")
 @SuppressWarnings("rawtypes")
 @Slf4j
+@CrossOrigin(origins="http://localhost:4200/")
 public class UserController {
 
     @Autowired
